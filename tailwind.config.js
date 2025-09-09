ffa8e1f563668c1c429b9a7ff3cb952f5e70c8d9
narindra <hei.narindra@gmail.com>
2025-09-09 15:22:27 +0300

    J'ai installé quelque package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
-
 module.exports = {
   darkMode: 'class', 
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
