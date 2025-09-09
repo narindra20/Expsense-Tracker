91c8737e2be6f6b758e6bcf24a4ef5a30646d747
Angela-Haim <hei.angela.5@gmail.com>
2025-09-09 11:17:18 +0300

    Modification

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,27 +1,20 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { PrismaClient } from '@prisma/client'; 
+import { PrismaClient } from "@prisma/client"; 
 import { authenticateToken } from "../middleware/middleware.js";
-
-
 
 const router = express.Router();
 const JWT_SECRET = "Fitiavana";
 const prisma = new PrismaClient(); 
 
-// SIGNUP
+//SIGNUP 
 router.post("/signup", async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
-  if (!firstName || !lastName || !email || !password)
+  if (!firstName || !lastName || !email || !password) {
     return res.json({ success: false, message: "Tous les champs sont requis" });
-
+  }
   try {
-    // Vérifie si l'email existe déjà
-    const exist = await pool.query("SELECT * FROM users WHERE email=$1", [email]);
-    if (exist.rows.length > 0)
-      return res.json({ success: false, message: "Email déjà utilisé" });
-
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await prisma.user.create({
@@ -44,8 +37,6 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-
-
 //LOGIN
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
@@ -57,10 +48,12 @@ router.post("/login", async (req, res) => {
     }
 
     const match = await bcrypt.compare(password, user.password);
-    if (!match)
+    if (!match) {
       return res.json({ success: false, message: "Email ou mot de passe incorrect" });
+    }
 
     const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "1h" });
+
     res.json({
       success: true,
       user: { id: user.id, firstName: user.firstName, email: user.email },
@@ -72,7 +65,7 @@ router.post("/login", async (req, res) => {
   }
 });
 
-// ROUTE PROTÉGÉE
+// ROUTE PROTÉGÉE 
 router.get("/dashboard", authenticateToken, (req, res) => {
   res.json({ message: `Bienvenue utilisateur ${req.user.userId}` });
 });
